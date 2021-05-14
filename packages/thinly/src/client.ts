dc7a0bbdb48d08fc1bbfb99870aca5364c8a7b1e
Steven Yung <stevenyung@fastmail.com>
2021-05-14 02:40:56 +0200

    (chore) remove unused extra console.log

--- packages/thinly/src/client.ts
+++ packages/thinly/src/client.ts
@@ -12,14 +12,10 @@ export function createClient(options) {
 
   const result = createMap(routes)
 
-  console.log({ result })
-
   return walk(result, [
     {
       match: (key) => key.startsWith(':'),
       handler: ({ routes, key, modifiers, depth, context }) => {
-        console.log({ acc: routes, key, where: '":" handler' })
-
         const methodName = key.slice(1)
 
         return {
@@ -39,8 +35,6 @@ export function createClient(options) {
     {
       match: (key) => key === '_routes',
       handler: ({ routes, key, context }) => {
-        console.log({ acc: routes, key, where: '"_routes" handler' })
-
         return routes[key].reduce((acc, route) => {
           if (context?.params) {
             route.path = Object.entries(context.params).reduce(
@@ -78,7 +72,6 @@ export function createClient(options) {
     {
       match: () => true,
       handler: ({ routes, key, modifiers, depth, context }) => {
-        console.log({ acc: routes, key, where: 'default' })
         return {
           ...routes,
           [key]: walk(routes[key], modifiers, depth + 1, context),
